850cec981fff6758e7e3da33e0443e47b4b2b11c
MikeShlapakov <76660778+MikeShlapakov@users.noreply.github.com>
2021-10-19 08:23:30 +0300

    Update server.py
    added mouse moving and clicking detection

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import numpy as np
 from random import randint
 import pyautogui
 from threading import Thread
+import win32api as win
 # PyQt5
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QPushButton, QAction, QMessageBox
@@ -15,8 +16,9 @@ from PyQt5.QtCore import QRect, Qt
 
 print("[SERVER]: STARTED")
 sock = socket.socket()
-sock.bind(('localhost', 9091)) # Your Server
+sock.bind(('192.168.31.186', 9091))  # Your Server
 sock.listen()
+print(sock.getsockname())
 conn, addr = sock.accept()
 
 class Dekstop(QMainWindow):
@@ -27,14 +29,28 @@ class Dekstop(QMainWindow):
     def ChangeImage(self):
         try:
             print("[SERVER]: CONNECTED: {0}!".format(addr[0]))
+            screenSize = conn.recv(1024)
+            print(screenSize)
+            screenSize = screenSize.decode()
+            self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, eval(screenSize)[0] // 2, eval(screenSize)[1] // 2))
+            self.setFixedSize(self.width(), self.height())
             while True:
                 img_bytes = conn.recv(999999)
                 self.pixmap.loadFromData(img_bytes)
                 self.label.setScaledContents(True)
                 self.label.resize(self.width(), self.height())
                 self.label.setPixmap(self.pixmap)
-                # if pyautogui.doubleClick():
-                #     if 
+                click = win.GetKeyState(0x01)
+                if click < 0:
+                    x, y = pyautogui.position()
+                    win_x,win_y,win_width,win_height = self.frameGeometry().x(), self.frameGeometry().y(), self.frameGeometry().width(), self.frameGeometry().height()
+                    if win_x<=x<=win_x+win_width and win_y<=y<=win_y+win_height:
+                        x = x-win_x
+                        y = y-win_y
+                        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+                        print(positionStr)
+                        # command = str([click, x*2, y*2])
+                        # conn.send(command.encode())
         except ConnectionResetError:
             QMessageBox.about(self, "ERROR", "[SERVER]: The remote host forcibly terminated the existing connection!")
             conn.close()
@@ -43,9 +59,10 @@ class Dekstop(QMainWindow):
         self.pixmap = QPixmap()
         self.label = QLabel(self)
         self.label.resize(self.width(), self.height())
-        self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, 800, 450))
-        # self.setFixedSize(self.width(), self.height())
         self.setWindowTitle("[SERVER] Remote Desktop: " + str(randint(99999, 999999)))
+        self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, 800, 400))
+        self.setFixedSize(self.width(), self.height())
+
         self.start = Thread(target=self.ChangeImage, daemon=True)
         self.start.start()
 
