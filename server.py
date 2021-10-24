f70be78668254753805928e8c3b2a5899fe9cd35
MikeShlapakov <76660778+MikeShlapakov@users.noreply.github.com>
2021-10-24 19:33:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import numpy as np
 from random import randint
 import pyautogui
 from threading import Thread
+import time
 import win32api as win
 import math
 # PyQt5
@@ -17,9 +18,8 @@ from PyQt5.QtCore import QRect, Qt
 
 print("[SERVER]: STARTED")
 sock = socket.socket()
-sock.bind(('192.168.31.186', 9091))  # Your Server
+sock.bind(('192.168.31.229', 9091))  # Your Server
 sock.listen()
-print(sock.getsockname())
 conn, addr = sock.accept()
 
 class Dekstop(QMainWindow):
@@ -27,45 +27,59 @@ class Dekstop(QMainWindow):
         super().__init__()
         self.initUI()
 
-    def ChangeImage(self):
+    def ScreenSharing(self):
         try:
             print("[SERVER]: CONNECTED: {0}!".format(addr[0]))
             screenSize = conn.recv(1024)
             print(screenSize)
             screenSize = screenSize.decode()
-            self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, eval(screenSize)[0] // 2, eval(screenSize)[1] // 2))
+            self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, eval(screenSize)[0] // 3, eval(screenSize)[1] // 3))
             self.setFixedSize(self.width(), self.height())
+
+            self.controlling = Thread(target=self.Controlling, args=(conn,), daemon=True)
+            self.controlling.start()
             while True:
                 img_bytes = conn.recv(999999)
                 self.pixmap.loadFromData(img_bytes)
                 self.label.setScaledContents(True)
                 self.label.resize(self.width(), self.height())
                 self.label.setPixmap(self.pixmap)
-                click = win.GetKeyState(0x01)
-                if click < 0:
-                    x, y = pyautogui.position()
-                    win_x,win_y,win_width,win_height = self.frameGeometry().x(), self.frameGeometry().y(), self.frameGeometry().width(), self.frameGeometry().height()
-                    if win_x<=x<=win_x+win_width and win_y<=y<=win_y+win_height:
-                        x = x-win_x
-                        y = y-win_y
-                        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-                        print(positionStr)
-                        command = str((x*2, y*2))
-                        conn.send(command.encode())
         except ConnectionResetError:
             QMessageBox.about(self, "ERROR", "[SERVER]: The remote host forcibly terminated the existing connection!")
             conn.close()
+
+    def Controlling(self, conn):
+        new_conn = socket.socket()
+        new_conn.bind(('192.168.31.229', 9092))  # Your Server
+        new_conn.listen()
+        conn, addr = new_conn.accept()
+        while True:
+            click = win.GetKeyState(0x01)
+            if click < 0:
+                x, y = pyautogui.position()
+                win_x, win_y = self.frameGeometry().x()+(self.frameGeometry().width()-self.label.width()), self.frameGeometry().y()+(self.frameGeometry().height()-self.label.height())
+                win_width, win_height = self.label.width(), self.label.height()
+                if win_x <= x <= win_x + win_width and win_y <= y <= win_y + win_height:
+                    print(win_x, win_y, win_width, win_height)
+                    print(x,y)
+                    x = x - win_x
+                    y = y - win_y
+                    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+                    print(positionStr)
+                    command = str((x * 3, y * 3))
+                    conn.send(command.encode())
+                    msg = conn.recv(256)
+                    print(msg)
 
     def initUI(self):
         self.pixmap = QPixmap()
         self.label = QLabel(self)
         self.label.resize(self.width(), self.height())
         self.setWindowTitle("[SERVER] Remote Desktop: " + str(randint(99999, 999999)))
-        self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, 800, 400))
-        self.setFixedSize(self.width(), self.height())
 
-        self.start = Thread(target=self.ChangeImage, daemon=True)
-        self.start.start()
+        self.screenSharing = Thread(target=self.ScreenSharing, daemon=True)
+        self.screenSharing.start()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
